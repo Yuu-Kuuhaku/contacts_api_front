84524399bb49cc8af5f6715074d39e962452544b
Eustaquio Ferreira <eusferalves@gmail.com>
2021-07-30 10:22:19 -0300

    modificações da função de geração

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,39 +33,45 @@ export class AppComponent {
     }
     const reader = new FileReader();
     reader.onload = (event) => {
-       
+
       const file: any = event?.target?.result;
       console.log(file)
-      const allLines = file.split(/\r\n|\n/);
+      let allLines= file.split(/\r\n|\n/);
       console.log(allLines)
+
       let count = 0;
       // Reading line by line
       let arrayJson = [];
       while (count < allLines.length) {
         const line = allLines[count];
-      
-        if (line.length === 78 && line.trim().match(/^\d/gm)) {
-            
-            let contrato = line.substring(0, 15).trim();
-            let dataLancamento = line.substring(15, 25);
-            let dataRemessa = line.substring(26, 36);
-            let lancamentos = line.substring(38, 75).trim()
-            let valor = allLines[count + 1].substring(40, 54).trim();
 
-            console.log(contrato)
-            console.log(dataLancamento)
-            console.log(dataRemessa)
-            console.log(lancamentos)
-            console.log(valor)
-            console.log(' ')
-            arrayJson.push({
-              contrato,
-              dataLancamento,
-              dataRemessa,
-              lancamentos,
-              valor
-            })
-            count += 2;
+        if (line.length === 78 && line.trim().match(/^\d/gm)) {
+
+          let contrato = line.substring(0, 15).trim();
+          let dataLancamento = line.substring(15, 25);
+          let dataRemessa = line.substring(26, 36);
+          let lancamentos = line.substring(38, 75).trim()
+          let nextLineCount = count + 1;
+
+          let nextLine = allLines[nextLineCount];
+          while(nextLine.length !== 78 && !nextLine.trim().match(/^\d/gm)) {
+              nextLineCount++;
+              nextLine = allLines[nextLineCount];
+          }
+
+          let valor = nextLine.substring(40, 54).trim();
+
+          console.log(`${count}, ${contrato}, ${dataLancamento}, ${dataRemessa}, ${lancamentos}, ${valor}`)
+          console.log(' ')
+          console.log(' ')
+          count = (nextLineCount + 1);
+          arrayJson.push({
+            contrato,
+            dataLancamento,
+            dataRemessa,
+            lancamentos,
+            valor
+          })
         } else {
             count += 1;
         }
